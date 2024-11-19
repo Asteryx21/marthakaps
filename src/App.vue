eff7c7e0130151fa8ce491bdd74e2dd7271de4cc
Vangelis Kapsoulakis <vangelis.kapsoulakis@learnworlds.com>
2024-11-19 11:49:53 +0200

    base nav

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,8 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <nav class="navbar">
+    <RouterLink to="/" class="nav-link" active-class="active-nav-link">Home</RouterLink>
+    <RouterLink to="/about" class="nav-link" active-class="active-nav-link">About</RouterLink>
+    <!-- <RouterLink to="/unsynced" class="nav-link">Unsynced List</RouterLink> -->
+  </nav>
   <RouterView />
 </template>
